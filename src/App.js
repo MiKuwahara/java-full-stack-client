8fa0197487b1b27f64650c9de0b094cb711e1c74
Michelle Kuwahara <mxk200037@utdallas.edu>
2023-11-27 16:01:48 -0600

    Initial commit for frontend. At this point, client can make HTTP Get request to retrieve all movies from the database using Axios. For this to work, we add @CrossOrigin(origins = "http://localhost:3000") inside the controller files and keep the backend running. Also, install all necessary dependencies for this project.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import api from "./api/axiosConfig.js";
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [movies, setMovies] = useState();
+
+    const getMovies = async () => {
+        try {
+            const response = await api.get("/api/v1/movies");
+
+            console.log(response.data);
+            console.log("Hello");
+
+            setMovies(response);
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getMovies();
+    }, []);
+
+    return (
+        <div className="App">
+
+        </div>
+    );
 }
 
 export default App;
