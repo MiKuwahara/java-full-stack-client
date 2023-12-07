911577c41618ae439403f1f37fa37456b255761b
Michelle Kuwahara <mxk200037@utdallas.edu>
2023-12-07 12:16:04 -0600

    Create Login and Register pages with starter code for testing for the following update => In Hero page,  add condition for review button. Only users who are already login will be able to post a review. Otherwise, they will be rerouted to login page.

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,4 +1,5 @@
 import './Hero.css';
+import {useState, useEffect} from "react";
 import Carousel from 'react-material-ui-carousel';
 import { Paper } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -9,6 +10,8 @@ import Button from 'react-bootstrap/Button';
 
 const Hero = ({movies}) => {
 
+    const [user, setUser] = useState();
+
     const navigate = useNavigate();
 
     function reviews(movieId)
@@ -16,6 +19,24 @@ const Hero = ({movies}) => {
         navigate(`/Reviews/${movieId}`);
     }
 
+    function login(){
+        navigate("/Login")
+    }
+
+
+    // execute when program first run
+    // this will get the "user" from local storage and 
+    // put it in state user
+    useEffect(() => {
+        setUser(JSON.parse(localStorage.getItem('user')));
+    },[]);
+
+    function isLogin(){
+        // Get user rom localStorage
+        const user = JSON.parse(localStorage.getItem('user'))
+        setUser(user);
+    }
+    
   return (
     <div className ='movie-carousel-container'>
       <Carousel>
@@ -41,9 +62,21 @@ const Hero = ({movies}) => {
                                             </div>
                                         </Link>
 
-                                        <div className="movie-review-button-container">
-                                            <Button variant ="info" onClick={() => reviews(movie.imdbId)} >Reviews</Button>
-                                        </div>
+                                        {/*if user exists (already login),
+                                        they will be able to post a review on a movie,
+                                        else will be rerouted to login page to login (or register) */}
+                                        { user? (
+                                             <div className="movie-review-button-container">
+                                             <Button variant ="info" onClick={() => reviews(movie.imdbId)} >Reviews</Button>
+                                         </div>
+                                        ):(
+                                            <div className="movie-review-button-container">
+                                            <Button variant ="info" onClick={() => login()} >Reviews</Button>
+                                            </div>
+                                        )
+
+                                        }
+                                       
                                     </div>
                                 </div>
                             </div>
